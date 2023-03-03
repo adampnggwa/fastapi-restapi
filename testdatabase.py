729d83fd5bd602f8111e04f09c81e0162c8bc03d
adampnggwa <126383639+adampnggwa@users.noreply.github.com>
2023-03-03 10:53:36 +0700

    Update testdatabase.py

--- testdatabase.py
+++ testdatabase.py
@@ -5,15 +5,15 @@ app = FastAPI()
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="yourusername",
-  password="yourpassword",
-  database="mydatabase"
+  user="root",
+  password="",
+  database="datauser"
 )
 
 mycursor = mydb.cursor()
 
 @app.get("/items/")
 async def read_items():
-    mycursor.execute("SELECT * FROM mytable")
+    mycursor.execute("SELECT * FROM siswa")
     result = mycursor.fetchall()
     return {"data": result}
