69fcaa37283f4d6c23294c97229d7ee5748ef712
adampnggwa <126383639+adampnggwa@users.noreply.github.com>
2023-03-12 15:49:04 +0700

    Update testdatabase.py

--- testdatabase.py
+++ testdatabase.py
@@ -37,8 +37,7 @@ async def tampilkan(request: Request):
     cursor.execute("SELECT * FROM siswa")
     atas = headers(request)
     results = cursor.fetchall()
-    db.close()
-    return JSONResponse(content={"hasil": hasil}, headers=atas)
+    return JSONResponse(content={"message": "data berhasil ditampilkan", "results": results}, headers=atas)
 
 # method get untuk mencari data berdasarkan ID
 @app.get("/user/{id}", tags=["Read Data"], summary="Mencari Data")
@@ -46,26 +45,22 @@ async def cari(id: int, request: Request):
     cursor.execute("SELECT * FROM siswa WHERE id = %s", (id,))
     atas = headers(request)
     results = cursor.fetchone()
-    db.close()
-    return JSONResponse(content={"hasil": hasil}, headers=atas)
+    return JSONResponse(content={"message": "data berhasil ditemukan", "result": results}, headers=atas)
 
 # method post untuk menambahkan data
 @app.post("/user/", tags=["Create Data"], summary="Menambah Data")
 async def tambah(data: DataModel, request: Request):
-    perintah.execute("INSERT INTO siswa (nama, hobi) VALUES (%s, %s)",
-    (data.nama, data.hobi))
+    cursor.execute("INSERT INTO siswa (nama, hobi) VALUES (%s, %s)", (data.nama, data.hobi))
     atas = headers(request)
     db.commit()
-    db.close()
     return JSONResponse(content={"message": "data berhasil ditambahkan"}, headers=atas)
 
 # method delete untuk menghapus data berdasarkan ID
 @app.delete("/user/{id}", tags=["Delete Data"], summary="Menghapus Data")
 async def hapus(id: int, request: Request):
-    perintah.execute("DELETE FROM siswa WHERE id = %s", (id,))
+    cursor.execute("DELETE FROM siswa WHERE id = %s", (id,))
     atas = headers(request)
     db.commit()
-    db.close()
     return JSONResponse(content={"massage": "data berhasil dihapus"}, headers=atas)
 
 # method patch untuk memperbarui data yang diinginkan
@@ -83,23 +78,21 @@ async def update(id: int, data: DataModel, request: Request):
     query += " WHERE id=%s"
     values.append(id)
     cursor.execute(query, tuple(values))
-    atas =headers(request)
+    atas = headers(request)
     db.commit()
-    db.close()
-    return JSONResponse(content={"massage": "data berhasil di perbarui"}, headers=atas)
+    return JSONResponse(content={"message": "data berhasil diperbarui"}, headers=atas)
     
 # method put untuk memperbarui seluruh data
 @app.put("/user/{id}", tags=["Update Data"], summary="Memperbarui Seluruh Data")
 async def update_all(id: int, data: DataModel, request: Request):
-    perintah.execute("UPDATE siswa SET nama = %s, hobi = %s WHERE id = %s",
+    cursor.execute("UPDATE siswa SET nama = %s, hobi = %s WHERE id = %s",
     (data.nama, data.hobi, id))
     atas = headers(request)
     db.commit()
-    db.close()
     return JSONResponse(content={"massage": "data berhasil di update"}, headers=atas)
 
 #detail keselahan
 @app.exception_handler(Exception)
 async def exception_handler(request: Request, exc: Exception):
     error_msg = f"Error occurred: {str(exc)}"
-    return JSONResponse(status_code=500, content={"message": error_msg})    
+    return JSONResponse(status_code=500, content={"message": error_msg})   
